cdf0c059ec95cc6898f22a27cbb97d8ad611b887
Vilius Garba <vilius.garba@gmail.com>
2022-09-12 21:17:51 +0300

    create working function to createGrid

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Time to script!
+// 3. fn to draw on grid
+// 4. fn to change colors
+// 5. fn to erase drawing
+const gridInput = document.getElementById("gridInput");
+
+function getGridSize() {
+  const gridInputValue = parseInt(gridInput.value, 10);
+  return gridInputValue;
+}
+
+function createGrid() {
+  const gridInputValue = getGridSize();
+  const gridContainer = document.querySelector(".gridContainer");
+  gridContainer.style.display = "grid";
+  gridContainer.style.gridTemplateColumns = `repeat(${gridInputValue}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridInputValue}, 1fr)`;
+
+  for (let i = 0; i < gridInputValue * gridInputValue; i++) {
+    const field = document.createElement("div");
+    gridContainer.appendChild(field);
+    field.addEventListener('hover',)
+  }
+}
+gridInput.addEventListener("change", createGrid());
+
+
