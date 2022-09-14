0da0d893e01b3da1e34a50089d56a21d1a455afd
Vilius Garba <vilius.garba@gmail.com>
2022-09-14 15:06:13 +0300

    implement drwa function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // 4. fn to change colors
 // 5. fn to erase drawing
 const gridInput = document.getElementById("gridInput");
+const gridContainer = document.querySelector(".gridContainer");
 
 function getGridSize() {
   const gridInputValue = parseInt(gridInput.value, 10);
@@ -10,15 +11,27 @@ function getGridSize() {
 
 function createGrid() {
   const gridInputValue = getGridSize();
-  const gridContainer = document.querySelector(".gridContainer");
+
   gridContainer.style.display = "grid";
   gridContainer.style.gridTemplateColumns = `repeat(${gridInputValue}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridInputValue}, 1fr)`;
 
   for (let i = 0; i < gridInputValue * gridInputValue; i++) {
     const field = document.createElement("div");
+    field.classList.add("field");
     gridContainer.appendChild(field);
   }
+
+  function draw() {
+    const allFields = gridContainer.querySelectorAll(".field");
+    allFields.forEach((field) => {
+      field.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "black";
+      });
+    });
+  }
+  draw();
 }
+
 gridInput.addEventListener("load", createGrid());
 gridInput.addEventListener("change", createGrid);
